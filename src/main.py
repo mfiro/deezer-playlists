21b274a9583329771d04496037b2038e79c2d246
mfiro <mfiro@protonmail.com>
2022-07-24 15:01:04 +0200

    [main.py] A draft of make pretty tables

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import deezer
 import json
 import os
 
+from helpers import get_dummy_tracks, list2prettyrow
+# TODO: Add pretty tables
+
+
 def get_catalog():
     """ get the catalog (list of playlists to get) """
     catalog_path = os.path.join(os.path.dirname(__file__), '../data/catalog/catalog.txt')
@@ -41,5 +45,40 @@ def main():
         print(f"Tracks' information saved to {save_path}")
 
 
+def dummy_main():
+    """ In order to prevent real api calls"""
+    
+    # get the catalog:
+    catalog = get_catalog()
+
+    # get some dummy data to start with
+    tracks = get_dummy_tracks(catalog)
+
+    # pretty table
+    column_names = ['Song', 'Artist', 'Album', 'Time']
+
+    page_content = f"Playlist title:{'Top Germany 100'} \n\n"
+
+    table = list2prettyrow(column_names) + '\n'
+    table += list2prettyrow(['---']*len(column_names)) +'\n'
+    for track in tracks:
+        table += list2prettyrow([track['title'],
+                                track['artist']['name'],
+                                track['album']['title'],
+                                track['duration']]) + '\n'
+
+    template_path = os.path.join(os.path.dirname(__file__), f'../data/playlists_pretty/table_template.md')
+    with open(template_path, 'w') as f:
+        f.write(page_content)
+        f.write(table)
+    
+    print(f"{template_path} updated")
+       
+
 if __name__ == "__main__":
-    main()
+    dummy_mode = True
+
+    if dummy_mode:
+        dummy_main()
+    else:
+        main()

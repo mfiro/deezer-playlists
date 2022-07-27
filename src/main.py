f84b0a8514b2bd496a92e12159daf2eed7ba8c4b
mfiro <mfiro@protonmail.com>
2022-07-27 21:20:32 +0200

    [main.py][archive.py] move archive related function to the newly created archive.py file

--- src/main.py
+++ src/main.py
@@ -4,36 +4,7 @@ import os
 
 from helpers import get_test_playlist
 from pretty_playlist import make_pretty_md
-
-def get_catalog():
-    """ get the catalog (list of playlists to get) """
-    catalog_path = os.path.join(os.path.dirname(__file__), '../data/catalog/catalog.txt')
-    with open(catalog_path, 'r') as f:
-        catalog = f.read().splitlines()
-    return catalog
-
-
-def save_as_json(playlist):
-    print(f"Saving track list ...")
-    save_path = os.path.join(os.path.dirname(__file__), f"../data/playlists/{playlist['id']}.json")
-    with open(save_path,'w') as f:
-        json.dump(playlist, f, indent=4)
-    print(f"Tracks' information saved to {save_path}")
-
-
-def save_as_pretty_table(playlist):
-
-    current_dir = os.path.dirname(__file__)
-    save_path = os.path.join(current_dir, f"../data/playlists_pretty/{playlist['id']}.md")
-
-    # get page content
-    page_content = make_pretty_md(playlist)
-
-    # Write to the md file
-    with open(save_path, 'w') as f:
-        f.write(page_content)
-    
-    print(f"{save_path} updated")
+from archive import get_catalog, save_as_json, save_as_pretty_table
 
 
 def main():

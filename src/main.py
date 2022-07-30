9e88a02d53c5b257c8124f291ce849461138c013
mfiro <mfiro@protonmail.com>
2022-07-30 14:30:52 +0200

    [archive.py][main.py] rename get_catalog -> read_catalog

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import deezer
 import os
 
 from src.helpers import get_test_playlist
-from src.archive import get_catalog, save_as_json, save_as_pretty_table
+from src.archive import read_catalog, save_as_json, save_as_pretty_table
 
 # TODO: rename get_catalog to read_catalog
 
@@ -11,13 +11,13 @@ def main():
 
     # get the catalog:
     catalog_path = os.path.join(data_dir, 'catalog/catalog.txt')
-    catalog = get_catalog(catalog_path)
+    playlist_ids = read_catalog(catalog_path)
 
     # initiate the deezer client
     client = deezer.Client()
 
     # get playlist info:
-    for id in catalog:
+    for id in playlist_ids:
         
         ## Destination paths:
         # json files location: ../data/playlists/id.json
@@ -46,10 +46,10 @@ def dummy_main():
     # get the catalog:
     data_dir = './data/'
     catalog_path = os.path.join(data_dir, 'test/catalog.txt')
-    catalog = get_catalog(catalog_path)
+    playlist_ids = read_catalog(catalog_path)
 
     # get some dummy data to start with
-    playlist = get_test_playlist(catalog, data_dir)
+    playlist = get_test_playlist(playlist_ids, data_dir)
 
     # save the template
     template_path = os.path.join(data_dir, f'playlists_pretty/table_template.md')
@@ -57,7 +57,7 @@ def dummy_main():
        
 
 if __name__ == "__main__":
-    dummy_mode = False
+    dummy_mode = True
 
     if dummy_mode:
         dummy_main()

c5e353900c0c014770f1dec5991629ce9b8ed9f4
mfiro <mfiro@protonmail.com>
2022-07-31 12:46:07 +0200

    [archive.py] Fix code style with flake8

--- deezerpl/archive.py
+++ deezerpl/archive.py
@@ -1,4 +1,3 @@
-import os
 import json
 from deezerpl.pretty_playlist import make_pretty_md
 
@@ -11,8 +10,8 @@ def read_catalog(catalog_path):
 
 
 def save_as_json(playlist, save_path):
-    print(f"Saving track list ...")
-    with open(save_path,'w') as f:
+    print("Saving track list ...")
+    with open(save_path, 'w') as f:
         json.dump(playlist, f, indent=4)
     print(f"Tracks' information saved to {save_path}")
 
@@ -24,5 +23,5 @@ def save_as_pretty_table(playlist, save_path):
     # Write to the md file
     with open(save_path, 'w') as f:
         f.write(page_content)
-    
+
     print(f"{save_path} updated")
